44c2710c88e0b3504abcbeb952e58aa1a7cbbca6
Paul Armer <paul.armer@autodesk.com>
2021-08-17 10:55:12 -0700

    deploy update test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,16 @@
 # Want this to stop if something fails
-rm -rf build \
-	&& mkdir build \
-	&& cp src/CNAME build/ \
-	&& cp src/*.css build/ \
-	&& cp src/*.html build/ \
-	&& cp -R src/media/ build/media/ \
-	&& gh-pages -d build
+
+rm -rf build
+mkdir build
+cp src/CNAME build/
+cp src/*.css build/
+cp src/*.html build/
+cp -R src/media/ build/media/
+cp src/styles/pixel-sans.* build/
+
+git checkout build
+mv build/* .
+# git add .
+# git commit -am "Deploying to production on `date`"
+# git push
+# git checkout main
