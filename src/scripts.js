78652cbcde6ee21613af7899319c5db7f937addb
Paul Armer <paul.armer@autodesk.com>
2021-08-17 00:50:32 -0700

    more content

--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,32 @@
-(function () {
+const initLinks = () => {
+	document.querySelectorAll('a').forEach(a => {
+		a.setAttribute('target', '_blank')
+	})
+}
+
+const initIcons = () => {
 	const ACTIVE_CLASS = 'active'
 
 	document.querySelectorAll("#desktop figure").forEach(fig => {
 		const name = fig.getAttribute('name')
-		console.log(`setting up ${name}`)
 
 		fig.onclick = () => {
-			console.log(`showing ${name}`)
 			document.querySelectorAll('#window article').forEach(art => {
 				art.classList.remove(ACTIVE_CLASS)
 			})
-			const activeArt = document.querySelector(`#window article[name='${name}']`)
-			if (activeArt) {
-				activeArt.classList.add(ACTIVE_CLASS)
-			} else {
-				console.error(`No window for ${name}!`)
-			}
+			const activeArt = document.querySelector(`#window article[name='${name}']`) || document.querySelector(`#window article[name='soon']`)
+			activeArt.classList.add(ACTIVE_CLASS)
 		}
 	})
+}
 
+const initToolbar = () => {
 	const startButton = document.querySelector('#toolbar button')
 	startButton.onclick = () => window.location.reload()
+}
+
+(function () {
+	initLinks()
+	initIcons()
+	initToolbar()
 })();
