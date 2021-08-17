e18aba335f45e56b15ebfbe9ad0cc1b87c8525f7
Paul Armer <paul.armer@autodesk.com>
2021-08-17 11:03:48 -0700

    Deploying to 'build' on Tue Aug 17 11:03:48 PDT 2021

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-# Want this to stop if something fails
-rm -rf build \
-	&& mkdir build \
-	&& cp src/CNAME build/ \
-	&& cp src/*.css build/ \
-	&& cp src/*.html build/ \
-	&& cp -R src/media/ build/media/ \
-	&& gh-pages -d build
