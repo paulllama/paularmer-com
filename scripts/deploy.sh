ebe19417df41781d92760185481ff1ffa5efc5da
Paul Llama <armerpaul@gmail.com>
2025-01-23 10:00:38 -0800

    update deploy to ignore node_modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ npm run build
 # Deploy to git
 git checkout build --
 git pull
-rm -rf `ls | grep -v build`
+rm -rf `ls | grep -v build | grep -v node_modules`
 mv build/* .
 rm -rf build/
 git add .
